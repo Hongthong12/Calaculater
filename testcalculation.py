d4048f76641665de7ae5dd9574be6ef407dc8ada
Hongthong12 <hfhongthong91@gmail.com>
2023-03-12 10:41:13 +0700

    secondcommit

---
+++ testcalculation.py
@@ -0,0 +1,19 @@
+input = input("Enter: ") #แรก
+for i in input:
+    if i == "+":
+        lst = input.split("+")
+        print(int(lst[0]) + int(lst[1]))
+
+    if i == "-":
+        lst = input.split("-")
+        print(int(lst[0]) - int(lst[1]))
+
+    if i == "*":
+        lst = input.split("*")
+        print(int(lst[0]) * int(lst[1]))
+
+    if i == "/":
+        lst = input.split("/")
+        print(int(lst[0]) / int(lst[1]))
+
+    print("----------------------")
