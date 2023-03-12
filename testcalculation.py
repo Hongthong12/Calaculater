03ff52f40d2abd0dbfa22bc14a6aac8cfb4f7a48
Hongthong12 <120234220+Hongthong12@users.noreply.github.com>
2023-03-12 23:21:29 +0700

    Update testcalculation.py

--- testcalculation.py
+++ testcalculation.py
@@ -1,12 +1,12 @@
-input = input("Enter: ") #แรกะะะะะะะะะะ
-for i in input:
+input = input("Enter: ") #แรกกำหนดตัวแปร "input"  เมือกด Enter  ให้ให้แปรคำตอบออกมา
+for i in input:          #
     if i == "+":
         lst = input.split("+")
-        print(int(lst[0]) + int(lst[1]))
+        print(int(lst[0]) + int(lst[1])) #
 
     if i == "-":
-        lst = input.split("-")
-        print(int(lst[0]) - int(lst[1]))
+        lst = input.split("-") 
+        print(int(lst[0]) - int(lst[1])) 
 
     if i == "*":
         lst = input.split("*")
