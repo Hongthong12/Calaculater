f7db699b5fb628a55720090847fd80f7df60ce61
Hongthong12 <120234220+Hongthong12@users.noreply.github.com>
2023-03-12 23:35:51 +0700

    Update testcalculation.py

--- testcalculation.py
+++ testcalculation.py
@@ -1,5 +1,5 @@
 input = input("Enter: ") #แรกกำหนดตัวแปร "input"  เมือกด Enter  ให้ให้แปรคำตอบออกมา
-for i in input:          #
+for i in input:          #สร้างเงื่อนไขและลูปเพื่อดำเนินการบวก ลบ คูณ และหาร
     if i == "+":
         lst = input.split("+")
         print(int(lst[0]) + int(lst[1])) #
